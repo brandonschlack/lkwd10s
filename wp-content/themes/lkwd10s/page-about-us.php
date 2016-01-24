51431b39b1b1da7a5252453e14a780dd23b1772b
brandonschlack <brandonschlack@gmail.com>
2016-01-23 17:31:34 -0800

    About Us and Events pages have template-parts

--- wp-content/themes/lkwd10s/page-about-us.php
+++ wp-content/themes/lkwd10s/page-about-us.php
@@ -8,12 +8,12 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'about-us' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
--- wp-content/themes/lkwd10s/page-events.php
+++ wp-content/themes/lkwd10s/page-events.php
@@ -8,20 +8,30 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container" role="main">
 
+		<?php
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header><!-- .page-header -->
 			<?php
+			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', 'events' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			endwhile;
 
-			endwhile; // End of the loop.
-			?>
+			the_posts_navigation();
+
+		endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
