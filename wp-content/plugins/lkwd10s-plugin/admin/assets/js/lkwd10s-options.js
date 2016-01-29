3c4aa50aa8e88d530c39e14f35b644cf292473b3
brandonschlack <brandonschlack@gmail.com>
2016-01-29 06:29:35 -0800

    Removed lkwd10s plugin

--- wp-content/plugins/lkwd10s-plugin/admin/assets/js/lkwd10s-options.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-    $('#upload_client_secret_button').click(function() {
-        tb_show('Upload Client Secret', 'media-upload.php?referer=lkwd10s-settings&type=fileTB_iframe=true&post_id=0', false);
-        return false;
-    });
-});
